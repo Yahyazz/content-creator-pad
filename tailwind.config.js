8535c251d0c7acb6e703d9cd2f316b419c7aa968
brilliantputritegar <brilliantputritegar@gmail.com>
2022-11-22 00:04:34 +0700

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config}**/
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      screens: {
+        xsm: "360px",
+      },
+      colors: {
+        primary_background: {
+          darkgray01: "#222831",
+          darkgray02: "#393E46",
+        },
+        primary: {
+          blue: "#00ADB5",
+        },
+      },
+      spacing: {
+        "76px": "76px",
+        "30px": "30px",
+        "116px": "116px",
+        "118px": "118px",
+        "300px": "300px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
